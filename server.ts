fe326ca9bd2c0a98da22c13310d7ebdac10cd807
beserion <hyk41hyk@gmail.com>
2026-03-31 11:25:28 +0300

    Canlıya alma

--- server.ts
+++ server.ts
@@ -36,7 +36,7 @@ const dbConfig: sql.config = {
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   app.use(express.json());
 
